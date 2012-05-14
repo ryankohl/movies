bdb1e519f9b2dcd88daf864eb546548539aa2506
Ryan Kohl <ryanckohl@gmail.com>
2012-05-14 07:48:44 -0400

    added slide deck

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Looking at movie data from LMDB"
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
-		 [seabass "0.4"]
+		 [seabass "0.5"]
 		 [incanter "1.2.3"]    
 		 [org.clojars.vgeshel/http.async.client "0.2.2"]])
